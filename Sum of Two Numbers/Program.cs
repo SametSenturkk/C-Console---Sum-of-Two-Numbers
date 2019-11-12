9f56d62e93d0f8e5ae2fa35495a0433fa8cad5db
Samet Şentürk <senturksamet123@gmail.com>
2019-11-12 18:12:15 +0300

    C# Console - Sum of Two Numbers

---
+++ Sum of Two Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Two_Numbers
+{
+    class Program
+    {
+      static void Main()
+        {
+            int number1, number2, total;
+            number1 = int.Parse(Console.ReadLine());
+            number2 = int.Parse(Console.ReadLine());
+            total = number1 + number2;
+            Console.Write("Total : " + total);
+            Console.ReadKey();
+        }
+    }
+}
